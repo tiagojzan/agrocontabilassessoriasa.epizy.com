5a6fa321ea6d3c24170d99d1c0afa703644b5329
tiagojzan <61213679+tiagojzan@users.noreply.github.com>
2021-11-07 16:50:27 -0300

    Create trocaimagem.js

---
+++ js/trocaimagem.js
@@ -0,0 +1,31 @@
+/////  TROCA IMAGEM QUANDO CURSOR MOUSE PASSA EM CIMA  \\\\\
+
+function trocaimagem01(imagem) {
+
+    imagem.src = "img/fachada perto.jpg";
+
+}
+
+
+
+function voltaimagem01(imagem) {
+
+    imagem.src = "img/fachada 01.jpg";
+
+}
+
+
+
+function trocaimagem02(imagem) {
+
+    imagem.src = "img/fachada rua.jpg";
+
+}
+
+
+
+function voltaimagem02(imagem) {
+
+    imagem.src = "img/fachada 02.jpg";
+
+}
