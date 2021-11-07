d1bd2dbccdb6ea3b400eb53bc08877d302f1d27a
tiagojzan <61213679+tiagojzan@users.noreply.github.com>
2021-11-07 16:50:42 -0300

    Add files via upload

---
+++ js/fade in.js
@@ -0,0 +1,21 @@
+/////// FADE IN - APARECE E DESAPARECE ELEMENTOS SITE \\\\\\\\\\\
+$(window).on("load", function() {
+    $(window).scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".scroll_fade_in_out").each(function() {
+            /* Verifique a localização de cada elemento desejado */
+            var objectBottom = $(this).offset().top + $(this).outerHeight();
+
+            /* Se o elemento estiver completamente dentro dos limites da janela, desvaneça-o */
+            if (objectBottom < windowBottom) { //O objeto entra em exibição (deslizando para baixo)
+                if ($(this).css("opacity") == 0) {
+                    $(this).fadeTo(500, 1); //500 = velocidade do fade in
+                }
+            } else { //O objeto fica fora de exibição (deslizando para cima)
+                if ($(this).css("opacity") == 1) {
+                    $(this).fadeTo(500, 0);
+                }
+            }
+        });
+    }).scroll(); //invocar scroll-handler na página-carregar
+});
---
+++ js/rolagem_liks.js
@@ -0,0 +1,38 @@
+/// VER FORMA DE COLOCAR LINK NO HEADER !!!!!
+/////// ROLAGEM SUAVE PELO SITE LINKS \\\\\\\\\\\
+
+// Selecione todos os links com hashes
+$('a[href*="#"]')
+    // Remova links que realmente não ligam para nada
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // Links na página
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Descobrir o elemento para se deslocar
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Existe um alvo de rolagem?
+            if (target.length) {
+                // Apenas evite o padrão se a animação realmente acontecer
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Ligar de volta após a animação
+                    // Deve mudar de foco!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Verificando se o alvo estava focado
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); //Adicionando tab index para elementos não focáveis
+                        $target.focus(); // Defina o foco novamente
+                    };
+                });
+            }
+        }
+    });
